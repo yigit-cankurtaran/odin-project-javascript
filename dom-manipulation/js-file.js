dfcbf0ab5f08562d56643a03a360ec1f3e20ee07
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-17 00:14:48 +0300

    Continued my tries on the Javascript file.

--- dom-manipulation/js-file.js
+++ dom-manipulation/js-file.js
@@ -3,5 +3,28 @@ let secondTry = document.getElementsByClassName("content")[0];
 // this logs the entire line including the <div> brackets
 let secondTryClean = document.getElementsByClassName("content")[0].innerHTML;
 // this only logs the text inside the html. INNER html.
+let secondTryCleanToo = document.getElementsByClassName("content")[0].textContent;
+// does the same thing.
 console.log(secondTry);
 console.log(secondTryClean);
+console.log(secondTryCleanToo);
+// let addTry = document.
+// this part might come in useful
+// const container = document.querySelector('#container');
+// console.dir(container);
+
+const newDiv = document.createElement('div');
+// these are ways we can manipulate this new element.
+// newDiv.style.color = 'blue';
+// newDiv.style.cssText = 'color: blue; background: white;';
+newDiv.setAttribute('style', 'color: blue; background: white;');
+newDiv.setAttribute('id', 'newest-div');
+newDiv.setAttribute('class', 'my-div');
+let newText = document.createTextNode("This just got added.");
+// didn't let me add a string by itself.
+newDiv.appendChild(newText);
+let anotherText = document.createTextNode("\tHello this is the 2nd addition.");
+newDiv.appendChild(anotherText);
+// gives an error when you do it this way.
+// document.appendChild(newDiv);
+document.body.appendChild(newDiv);
