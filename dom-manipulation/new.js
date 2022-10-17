0290da6530c22102a54cdd686902579572b110e5
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-17 23:04:56 +0300

    Finished the h3 addition.

--- dom-manipulation/new.js
+++ dom-manipulation/new.js
@@ -1,7 +1,11 @@
 // adding the first (red <p>) bullet point
 const newP = document.createElement('p');
 newP.setAttribute('style', 'color: red;');
-newP.textContent= "Hey I'm red!";
+newP.textContent = "Hey I'm red!";
 document.body.appendChild(newP);
 
 // adding the second (blue <h3>) bullet point
+const newH = document.createElement('h3');
+newH.setAttribute('style', 'color: blue;');
+newH.textContent = "I'm a blue h3!";
+document.body.appendChild(newH);
