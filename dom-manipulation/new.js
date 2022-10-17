f7ac7fcd86f8b39c1510928e76aefddc21be7733
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-17 23:11:44 +0300

    Rewrote the container code according to the DRY method.

--- dom-manipulation/new.js
+++ dom-manipulation/new.js
@@ -1,12 +1,14 @@
+const container = document.getElementById('container');
+
 // adding the first (red <p>) bullet point
 const newP = document.createElement('p');
 newP.setAttribute('style', 'color: red;');
 newP.textContent = "Hey I'm red!";
 // adding container by itself didn't work the first time, let me try again.
-document.getElementById('container').appendChild(newP);
+container.appendChild(newP);
 
 // adding the second (blue <h3>) bullet point
 const newH = document.createElement('h3');
 newH.setAttribute('style', 'color: blue;');
 newH.textContent = "I'm a blue h3!";
-document.container.appendChild(newH);
+container.appendChild(newH);
