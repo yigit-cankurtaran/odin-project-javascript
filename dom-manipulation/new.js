5f4292cd3e2aaf7ddacba6f8bba5351a5c3d251c
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-17 23:18:41 +0300

    Fixed the comments

--- dom-manipulation/new.js
+++ dom-manipulation/new.js
@@ -1,14 +1,17 @@
+// implementing the container div
 const container = document.getElementById('container');
 
-// adding the first (red <p>) bullet point
+// adding the first (red <p>) element
 const newP = document.createElement('p');
 newP.setAttribute('style', 'color: red;');
 newP.textContent = "Hey I'm red!";
 // adding container by itself didn't work the first time, let me try again.
 container.appendChild(newP);
 
-// adding the second (blue <h3>) bullet point
+// adding the second (blue <h3>) element
 const newH = document.createElement('h3');
 newH.setAttribute('style', 'color: blue;');
 newH.textContent = "I'm a blue h3!";
 container.appendChild(newH);
+
+// adding the third (black border, pink background <div>) element
