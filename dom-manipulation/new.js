f4e9df34d1fe89c5f858f911791473c97a940278
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-17 23:02:59 +0300

    Added comments on the script file.

--- dom-manipulation/new.js
+++ dom-manipulation/new.js
@@ -1,4 +1,7 @@
+// adding the first (red <p>) bullet point
 const newP = document.createElement('p');
 newP.setAttribute('style', 'color: red;');
 newP.textContent= "Hey I'm red!";
 document.body.appendChild(newP);
+
+// adding the second (blue <h3>) bullet point
