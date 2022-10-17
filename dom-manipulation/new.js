006bf4c3f6edcf451926cfb0d3233b0da22630e6
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-17 23:10:21 +0300

    Fixed the container-body situation.

--- dom-manipulation/new.js
+++ dom-manipulation/new.js
@@ -1,15 +1,12 @@
-// writing in the container element.
-const container = document.querySelector('#container');
-
-
 // adding the first (red <p>) bullet point
 const newP = document.createElement('p');
 newP.setAttribute('style', 'color: red;');
 newP.textContent = "Hey I'm red!";
-document.body.appendChild(newP);
+// adding container by itself didn't work the first time, let me try again.
+document.getElementById('container').appendChild(newP);
 
 // adding the second (blue <h3>) bullet point
 const newH = document.createElement('h3');
 newH.setAttribute('style', 'color: blue;');
 newH.textContent = "I'm a blue h3!";
-document.body.appendChild(newH);
+document.container.appendChild(newH);
