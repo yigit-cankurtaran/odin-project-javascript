1ea6127e5ec6c3d0e74e847ad70ae1842a68f23e
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-18 20:56:27 +0300

    Figuring out the button onclick method.

--- dom-manipulation/dynamic-manipulation/js-trial.js
+++ dom-manipulation/dynamic-manipulation/js-trial.js
@@ -8,3 +8,14 @@ list.appendChild(second);
 document.body.appendChild(list);
 
 list.addEventListener("click", console.log("pog!"));
+// this creates the effect we want at the start and never again.
+
+// going to try out a button now.
+const btn = document.createElement('button');
+btn.textContent = "Would you kindly click me?";
+document.body.appendChild(btn);
+// created the button.
+btn.onclick = () => document.body.setAttribute("style", "background-color: black");
+btn.onclick = () => alert("Hello World!");
+// it does whatever the last onclick element says.
+// because it manipulates the html of the button element itself, doesn't add a new function to the code.
