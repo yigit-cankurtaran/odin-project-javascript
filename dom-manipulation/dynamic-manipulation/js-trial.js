20dbabe1c6c82e6b260ec0b4042c6381cc3cbe69
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-18 21:23:45 +0300

    Learned the event listener method to manipulate elements.

--- dom-manipulation/dynamic-manipulation/js-trial.js
+++ dom-manipulation/dynamic-manipulation/js-trial.js
@@ -20,4 +20,13 @@ btn.onclick = () => alert("Hello World!");
 // it does whatever the last onclick element says.
 // because it manipulates the html of the button element itself, doesn't add a new function to the code.
 btn.onclick = () => document.body.style.backgroundColor == "white" ? document.body.setAttribute("style", "background-color: black") : document.body.setAttribute("style", "background-color: white");
-// this is how i can implement a dark mode.
+// this is how i could implement a dark mode.
+const newBtn = document.createElement('button');
+newBtn.textContent = "Click me not the one next to me";
+document.body.appendChild(newBtn);
+newBtn.addEventListener('click', () => {
+  alert("Hello World!");
+  console.log("I should be able to do multiple things, let's see.");
+});
+// this is a better way to do it all
+// you can do multiple things without one overriding the other.
