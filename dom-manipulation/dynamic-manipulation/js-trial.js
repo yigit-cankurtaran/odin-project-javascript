38e97bad6545f00abd081ed95d6823c57025e360
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-18 20:36:52 +0300

    Fixed placeholders.

--- dom-manipulation/dynamic-manipulation/js-trial.js
+++ dom-manipulation/dynamic-manipulation/js-trial.js
@@ -1,9 +1,9 @@
 const list = document.createElement('li');
-const first = document.createTextNode("Henloooo uwuwuwu");
-const second = document.createTextNode("I'm Saul Goodman he/they!!!");
+const first = document.createTextNode("first thing");
+const second = document.createTextNode("second thing");
 list.setAttribute("style", "color: red; border-style: solid; border-color: black;");
 list.appendChild(first);
 list.appendChild(second);
 document.body.appendChild(list);
 
-list.onclick(console.log("Hello World!"));
+list.addEventListener("click", console.log("pog!"));
