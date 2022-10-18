2f35e635f0bd9676c5203b580fe4c6cc9a9e3743
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-10-18 20:46:41 +0300

    Testing out list manipulation.

--- dom-manipulation/dynamic-manipulation/js-trial.js
+++ dom-manipulation/dynamic-manipulation/js-trial.js
@@ -1,6 +1,7 @@
 const list = document.createElement('li');
 const first = document.createTextNode("first thing");
-const second = document.createTextNode("second thing");
+const second = document.createTextNode("\nsecond thing");
+// doesn't create a new line element for some reason.
 list.setAttribute("style", "color: red; border-style: solid; border-color: black;");
 list.appendChild(first);
 list.appendChild(second);
